6ec971172796423efead1ec98cab0a25e09efd58
Alex Popescu <the.mindstorm@gmail.com>
2015-12-17 08:24:54 -0800

    remove forgotten fmt.Printf

--- cassandra/types.go
+++ cassandra/types.go
@@ -29,12 +29,13 @@ func (d *Date) String() string {
 
 var Epoch = time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
 
+// Create a new Date.
 func NewDate(year int, month time.Month, day int) *Date {
 	date := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
 	d := date.Sub(Epoch)
 	var days int64 = int64(d.Hours()) / 24
 	var udays = uint32(days + math.MaxInt32 + 1)
-	fmt.Printf("days: %d\n", udays)
+
 	return &Date{udays}
 }
 
@@ -67,7 +68,6 @@ func ParseUUID(s string) (uuid UUID, err error) {
 		}
 	}
 
-	// fmt.Printf("Ce kilu: %s\n", s)
 	b := []byte(s)
 	u := uuid[:]
 	_, err = hex.Decode(u[0:4], b[0:8])
